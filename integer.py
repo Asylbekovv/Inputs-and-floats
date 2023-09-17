f2c7e4ffeefc8282e8bf81ceabd2000427c3655e
Asylbekovv <144426976+Asylbekovv@users.noreply.github.com>
2023-09-17 23:20:23 +0600

    Add files via upload

---
+++ integer.py
@@ -0,0 +1,39 @@
+# Edit numbers 
+any_num = input("Enter your number: ")
+print(any_num)
+print(type(any_num))
+user_num = input("Enter any num: ")
+another_num = int(user_num)
+print(type(another_num))
+
+user2_num = int(input("Enter your second number: "))
+print(type(user2_num))
+
+
+one_million = 1_000_000
+print(one_million)
+
+first_num = 20
+second_num = 2
+print(pow(first_num, second_num))
+# Work with inputs and int
+input_str = input("Enter your number: ")
+input_int = int(input("Enter your second number: "))
+print(input_int)
+print(type(input_int))
+print(input_str)
+print(type(input_str))
+
+
+my_first_pow = 10
+my_second_pow = 11
+result_pow = my_first_pow, my_second_pow
+print(result_pow)
+print(type(result_pow))
+# Long integer
+long_int = 1000_000_000
+any_long_int = input("Enter your int: ")
+other_int = str(any_long_int)
+print(other_int)
+print(type(other_int))
+
