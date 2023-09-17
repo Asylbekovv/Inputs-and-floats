55f7f49a3abd76e4b37d9bbb78e71b1864601035
Asylbekovv <144426976+Asylbekovv@users.noreply.github.com>
2023-09-17 23:45:15 +0600

    Add files via upload

---
+++ floats.py
@@ -0,0 +1,23 @@
+average_float = 23.66
+print(average_float)
+print(type(average_float))
+my_int = input("Enter integer num: ")
+any_int = float(input("Enter your edit int: "))
+print(any_int)
+print(type(any_int))
+
+my_float = float(input("Enter your float num: "))
+any_float = int(my_float)
+print(any_float)
+print(type(any_float))
+
+# Round sum
+my_round_sum = 22.55
+print(round(my_round_sum))
+print(type(round(my_round_sum)))
+my_second_sum = 22
+print(round(my_second_sum))
+
+
+
+
